97e3f67482cc0554c6754ba28997e788156cbe9c
Styunlen <30810222+Styunlen@users.noreply.github.com>
2020-10-05 04:15:05 +0800

    1.1.0 build-74
    1.1.0 build-74发布
    更新内容在UpdateLog中

--- 高考倒计时/main.cpp
+++ 高考倒计时/main.cpp
@@ -12,6 +12,11 @@ int uimain(std::function<int()> run) {
 	sciter::archive::instance().open(aux::elements_of(resources)); // bind resources[] (defined in "resources.cpp") with the archive
 	MainWindow *pwin = new MainWindow();
 	SciterSetOption(pwin->get_hwnd(), SCITER_ALPHA_WINDOW, TRUE);
+	SciterSetOption(NULL, SCITER_SET_SCRIPT_RUNTIME_FEATURES,
+		ALLOW_FILE_IO |
+		ALLOW_SOCKET_IO |
+		ALLOW_EVAL |
+		ALLOW_SYSINFO);
 	::pwin = pwin;
 	
 	// note: this:://app URL is dedicated to the sciter::archive content associated with the application
