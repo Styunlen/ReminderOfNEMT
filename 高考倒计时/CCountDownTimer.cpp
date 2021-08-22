f1edd11a746cdff09552aa417a985cb0ff65ee05
Styunlen <30810222+Styunlen@users.noreply.github.com>
2021-08-22 23:38:04 +0800

    8.22中间代码备份
    中间代码备份
    修复了几个bug

--- 高考倒计时/CCountDownTimer.cpp
+++ 高考倒计时/CCountDownTimer.cpp
@@ -10,6 +10,7 @@
 ********************/
 MainWindow *pwin = nullptr;
 string g_iniPath = getCurrentWorkDir() + "\\options.ini";
+string g_themeOptionPath = getCurrentWorkDir() + "\\theme.ini";
 
 /********************
 	普通方法定义区
@@ -34,8 +35,34 @@ char * WsGetErrorInfo()
 		0,
 		NULL);
 	if (retSize > 0) {
-		pTemp[strlen((pTemp)) - 2] = '\0'; //remove cr and newline character
-		sprintf_s(pBuf, bufSize, "%0.*s", bufSize, pTemp);
+		pTemp[wcslen(pTemp) - 2] = '\0'; //remove cr and newline character
+		sprintf_s(pBuf, bufSize, "%0.*ws", bufSize, pTemp);
+		LocalFree((HLOCAL)pTemp);
+	}
+	return pBuf;
+}
+
+char * WsGetErrorInfo(DWORD err)
+{
+	static CHAR pBuf[1024] = { 0 };
+	const ULONG bufSize = 1024;
+	DWORD retSize;
+	LPTSTR pTemp = NULL;
+
+	pBuf[0] = '0';
+
+	retSize = FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER |
+		FORMAT_MESSAGE_FROM_SYSTEM |
+		FORMAT_MESSAGE_ARGUMENT_ARRAY,
+		NULL,
+		err,
+		LANG_NEUTRAL,
+		(LPTSTR)&pTemp,
+		0,
+		NULL);
+	if (retSize > 0) {
+		pTemp[wcslen(pTemp) - 2] = '\0'; //remove cr and newline character
+		sprintf_s(pBuf, bufSize, "%0.*ws", bufSize, pTemp);
 		LocalFree((HLOCAL)pTemp);
 	}
 	return pBuf;
@@ -53,21 +80,44 @@ string	getCurrentWorkDir()
 	return ApplicationPath;
 }
 
-string getProfileOptions(char* appName,char* keyName)
+string getProfileOptions(const char* appName, const char* keyName, string fileLocation)
 {
 	char * temp = new char[513];
 	//WritePrivateProfileString("UserInfo", "WindowStyle", "Circle", g_iniPath.c_str());
-	GetPrivateProfileStringA(appName, keyName, "NULL", temp, 512, g_iniPath.c_str());
+	GetPrivateProfileStringA(appName, keyName, "NULL", temp, 512, fileLocation.c_str());
 	string res = temp;
 	delete[] temp;
 	if (res == "NULL")
 		debugLogs(WsGetErrorInfo());
 	return res;
 }
-string setProfileOptions(char* appName, char* keyName, string value)
+
+wstring getProfileOptions(const wchar_t* appName, const wchar_t* keyName, string fileLocation)
+{
+	wchar_t * temp = new wchar_t[513];
+	//WritePrivateProfileString("UserInfo", "WindowStyle", "Circle", g_iniPath.c_str());
+	GetPrivateProfileStringW(appName, keyName, L"NULL", temp, 512, CharToWchar(fileLocation.c_str()).c_str());
+	wstring res = temp;
+	delete[] temp;
+	if (res == L"NULL")
+		debugLogs(WsGetErrorInfo());
+	return res;
+}
+
+string setProfileOptions(const char* appName, const char* keyName, string value,string fileLocation)
 {
 	//WritePrivateProfileString("UserInfo", "WindowStyle", "Circle", g_iniPath.c_str());
-	bool ret = WritePrivateProfileStringA(appName, keyName, value.c_str(), g_iniPath.c_str());
+	bool ret = WritePrivateProfileStringA(appName, keyName, value.c_str(), fileLocation.c_str());
+	string res = "SUCCESS";
+	if (!ret)
+		res = WsGetErrorInfo();
+	return res;
+}
+
+string setProfileOptions(const wchar_t* appName, const wchar_t* keyName, wstring value, string fileLocation)
+{
+	//WritePrivateProfileString("UserInfo", "WindowStyle", "Circle", g_iniPath.c_str());
+	bool ret = WritePrivateProfileStringW(appName, keyName, value.c_str(), CharToWchar(fileLocation.c_str()).c_str());
 	string res = "SUCCESS";
 	if (!ret)
 		res = WsGetErrorInfo();
@@ -82,9 +132,9 @@ bool getDebugMode()
 	return ret;
 }
 
-string getWindowStyle()
+string getThemeName()
 {
-	return getProfileOptions("UserInfo", "WindowStyle");
+	return getProfileOptions("UserInfo", "Theme");
 }
 
 string getTime()
@@ -92,7 +142,7 @@ string getTime()
 	return getProfileOptions("UserInfo", "Time");
 }
 
-std::string WcharToChar(const wchar_t* wp, size_t m_encode = CP_ACP)
+std::string WcharToChar(const wchar_t* wp, size_t m_encode)
 {
 	std::string str;
 	int len = WideCharToMultiByte(m_encode, 0, wp, wcslen(wp), NULL, 0, NULL, NULL);
@@ -104,12 +154,24 @@ std::string WcharToChar(const wchar_t* wp, size_t m_encode = CP_ACP)
 	return str;
 }
 
+std::wstring CharToWchar(const char* c, size_t m_encode)
+{
+	std::wstring str;
+	int len = MultiByteToWideChar(m_encode, 0, c, strlen(c), NULL, 0);
+	wchar_t*	m_wchar = new wchar_t[len + 1];
+	MultiByteToWideChar(m_encode, 0, c, strlen(c), m_wchar, len);
+	m_wchar[len] = '\0';
+	str = m_wchar;
+	delete m_wchar;
+	return str;
+}
+
 /********************
 	类方法定义区
 ********************/
 sciter::value MainWindow::Test() {
 #ifdef  _DEBUG
-	MessageBox(NULL, "Test", NULL, NULL);
+	MessageBoxA(NULL, "Test", NULL, NULL);
 #endif //  _DEBUG
 
 	return sciter::value("NULL");
@@ -120,9 +182,9 @@ sciter::value MainWindow::NA_getWorkDir()
 	return sciter::value(getCurrentWorkDir());
 }
 
-sciter::value MainWindow::NA_getStyle()
+sciter::value MainWindow::NA_getThemeName()
 {
-	return sciter::value(getWindowStyle());
+	return sciter::value(getThemeName());
 }
 
 sciter::value MainWindow::NA_getTime()
@@ -130,9 +192,9 @@ sciter::value MainWindow::NA_getTime()
 	return sciter::value(getTime());
 }
 
-sciter::value MainWindow::NA_setStyle(sciter::value style)
+sciter::value MainWindow::NA_setThemeName(sciter::value themeName)
 {
-	return sciter::value(setProfileOptions("UserInfo","WindowStyle", WcharToChar(style.to_string().c_str())));
+	return sciter::value(setProfileOptions("UserInfo","Theme", WcharToChar(themeName.to_string().c_str())));
 }
 
 sciter::value MainWindow::NA_setTime(sciter::value time)
@@ -150,11 +212,22 @@ sciter::value MainWindow::NA_setPos(sciter::value pos)
 	return sciter::value(setProfileOptions("UserInfo", "WindowPos", WcharToChar(pos.to_string().c_str())));
 }
 
+sciter::value MainWindow::NA_saveThemeOption(sciter::value themeName, sciter::value optionName, sciter::value optionValue)
+{
+	wstring wstr_optVal = optionValue.to_string().c_str();
+	return sciter::value(setProfileOptions(themeName.to_string().c_str(),optionName.to_string().c_str(), wstr_optVal,g_themeOptionPath));
+}
+
+sciter::value MainWindow::NA_getThemeOption(sciter::value themeName, sciter::value optionName)
+{
+	return sciter::value(getProfileOptions(themeName.to_string().c_str(), optionName.to_string().c_str(), g_themeOptionPath));
+}
+
 void MainWindow::onReady()
 {
 	if (getDebugMode())
 	{
 		static sciter::debug_output_console console; //- uncomment it if you will need console window
-		pwin->call_function("IA_uiDebugMode", sciter::value(true));
+		//pwin->call_function("IA_uiDebugMode", sciter::value(true));
 	}
 }
--- 高考倒计时/CCountDownTimer.h
+++ 高考倒计时/CCountDownTimer.h
@@ -32,8 +32,18 @@ extern HINSTANCE ghInstance;
 /********************
 	普通方法声明区
 ********************/
+char * WsGetErrorInfo();
+char * WsGetErrorInfo(DWORD err);
 string	getCurrentWorkDir();
-string getWindowStyle();
+string getProfileOptions(const char* appName, const char* keyName, string fileLocation = g_iniPath);
+wstring getProfileOptions(const wchar_t* appName, const wchar_t* keyName, string fileLocation = g_iniPath);
+string setProfileOptions(const char* appName, const char* keyName, string value, string fileLocation = g_iniPath);//执行成功返回大写的"SUCCESS"
+string setProfileOptions(const wchar_t* appName, const wchar_t* keyName, wstring value, string fileLocation = g_iniPath);
+bool getDebugMode();
+string getThemeName();
+string getTime();
+std::string WcharToChar(const wchar_t* wp, size_t m_encode = CP_ACP);
+std::wstring CharToWchar(const char* c, size_t m_encode = CP_ACP);
 
 
 /********************
@@ -45,23 +55,27 @@ public:
 BEGIN_FUNCTION_MAP
 	FUNCTION_0("Test", Test);
 	FUNCTION_0("NA_getWorkDir", NA_getWorkDir); //NA means native
-	FUNCTION_0("NA_getStyle", NA_getStyle);
+	FUNCTION_0("NA_getThemeName", NA_getThemeName);
 	FUNCTION_0("NA_getTime", NA_getTime);
-	FUNCTION_1("NA_setStyle", NA_setStyle);
+	FUNCTION_1("NA_setThemeName", NA_setThemeName);
 	FUNCTION_1("NA_setTime", NA_setTime);
 	FUNCTION_0("NA_getPos", NA_getPos);
 	FUNCTION_1("NA_setPos", NA_setPos);
+	FUNCTION_3("NA_saveThemeOption", NA_saveThemeOption);
+	FUNCTION_2("NA_getThemeOption", NA_getThemeOption);
 END_FUNCTION_MAP
-	sciter::value  Test();
+	sciter::value Test();
 	sciter::value NA_getWorkDir();
-	sciter::value NA_getStyle();
+	sciter::value NA_getThemeName();
 	sciter::value NA_getTime();
-	sciter::value NA_setStyle(sciter::value style);
+	sciter::value NA_setThemeName(sciter::value themeName);
 	sciter::value NA_setTime(sciter::value time);
 	sciter::value NA_getPos();
 	sciter::value NA_setPos(sciter::value pos);
+	sciter::value NA_saveThemeOption(sciter::value themeName, sciter::value optionName, sciter::value optionValue);
+	sciter::value NA_getThemeOption(sciter::value themeName,sciter::value optionName);
 	void onReady();
-	MainWindow() : window(SW_ALPHA | SW_MAIN | SW_ENABLE_DEBUG) {}
+	MainWindow() : window(SW_TITLEBAR | SW_MAIN | SW_ENABLE_DEBUG) {}
 };
 
 #endif
